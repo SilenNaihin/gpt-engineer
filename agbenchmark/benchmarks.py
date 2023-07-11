cde9be3e73212b3d8366a4ed149a18122bfe2333
Silen Naihin <silen.naihin@gmail.com>
2023-07-10 21:35:20 -0400

    add reports, consolidate, update benchmark files

--- agbenchmark/benchmarks.py
+++ agbenchmark/benchmarks.py
@@ -6,24 +6,24 @@ import pexpect
 
 def run_specific_agent(task: str) -> Tuple[str, int]:
     # Ensure the directory for the project exists
-    os.makedirs('projects/my-new-project', exist_ok=True)
+    os.makedirs("projects/my-new-project", exist_ok=True)
 
     # Create/overwrite the prompt file with the task
-    with open('projects/my-new-project/prompt', 'w') as prompt_file:
+    with open("projects/my-new-project/prompt", "w") as prompt_file:
         prompt_file.write(task)
 
     # Run the gpt-engineer command
-    child = pexpect.spawn('gpt-engineer projects/my-new-project')
+    child = pexpect.spawn("gpt-engineer projects/my-new-project")
 
     # Send 'c' and 'enter' keys
-    child.sendline('c')
+    child.sendline("c")
     child.sendline()
     child.sendline()
 
     # Create a loop to continuously read output
     while True:
         try:
-            child.expect('\n')  # This waits until a newline appears
+            child.expect("\n")  # This waits until a newline appears
             print(child.before.decode())  # This prints the line
         except pexpect.EOF:
             break  # No more output, break the loop
@@ -33,6 +33,7 @@ def run_specific_agent(task: str) -> Tuple[str, int]:
 
     # Return child process's exit status and any error messages
     return child.before.decode(), child.exitstatus
+
 
 if __name__ == "__main__":
     # The first argument is the script name itself, second is the task
